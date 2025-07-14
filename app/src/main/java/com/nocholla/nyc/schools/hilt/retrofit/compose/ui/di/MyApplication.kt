b6ab72e9810f0f061c2bb337cdb1473ef86a8147
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2025-07-13 18:29:42 -0700

    Added MyApplication

---
+++ app/src/main/java/com/nocholla/nyc/schools/hilt/retrofit/compose/ui/di/MyApplication.kt
@@ -0,0 +1,7 @@
+package com.nocholla.nyc.schools.hilt.retrofit.compose.ui.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application()
