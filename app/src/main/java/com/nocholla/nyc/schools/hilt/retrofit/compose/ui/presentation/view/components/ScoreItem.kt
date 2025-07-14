8f4c25f22babbf0bdc150f7ac731c3f3ef520ba7
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2025-07-13 19:51:51 -0700

    Update ScoreItem.kt

--- app/src/main/java/com/nocholla/nyc/schools/hilt/retrofit/compose/ui/presentation/view/components/ScoreItem.kt
+++ app/src/main/java/com/nocholla/nyc/schools/hilt/retrofit/compose/ui/presentation/view/components/ScoreItem.kt
@@ -1,4 +1,19 @@
 package com.nocholla.nyc.schools.hilt.retrofit.compose.ui.presentation.view.components
 
-class ScoreItem {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.nocholla.nyc.schools.hilt.retrofit.compose.ui.domain.model.Score
+
+@Composable
+fun ScoreItem(score: Score) {
+    Column(modifier = Modifier.padding(8.dp)) {
+        Text(text = score.schoolName)
+        Text(text = "Math: ${score.satMathAvgScore}")
+        Text(text = "Reading: ${score.satCriticalReadingAvgScore}")
+        Text(text = "Writing: ${score.satWritingAvgScore}")
+    }
 }
