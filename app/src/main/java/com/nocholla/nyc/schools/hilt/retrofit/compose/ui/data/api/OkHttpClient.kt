a4b448130977038d413adacd0d477a8d0b21a3f4
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2025-07-13 19:18:38 -0700

    Added images

--- app/src/main/java/com/nocholla/nyc/schools/hilt/retrofit/compose/ui/data/api/OkHttpClient.kt
+++ app/src/main/java/com/nocholla/nyc/schools/hilt/retrofit/compose/ui/data/api/OkHttpClient.kt
@@ -1,4 +1,22 @@
 package com.nocholla.nyc.schools.hilt.retrofit.compose.ui.data.api
 
-class OkHttpClient {
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class OkHttpClientFactory @Inject constructor() {
+    fun createOkHttpClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
 }
