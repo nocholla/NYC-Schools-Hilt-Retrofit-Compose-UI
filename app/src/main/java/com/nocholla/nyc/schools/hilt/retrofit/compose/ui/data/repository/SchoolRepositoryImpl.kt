be67d2bae2189bebbaeee79ee59228ca5a67b0e6
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2025-07-13 19:44:02 -0700

    Update SchoolRepositoryImpl.kt

--- app/src/main/java/com/nocholla/nyc/schools/hilt/retrofit/compose/ui/data/repository/SchoolRepositoryImpl.kt
+++ app/src/main/java/com/nocholla/nyc/schools/hilt/retrofit/compose/ui/data/repository/SchoolRepositoryImpl.kt
@@ -1,4 +1,24 @@
 package com.nocholla.nyc.schools.hilt.retrofit.compose.ui.data.repository
 
-class SchoolRepositoryImpl {
+import com.nocholla.nyc.schools.hilt.retrofit.compose.ui.data.api.SchoolsApiService
+import com.nocholla.nyc.schools.hilt.retrofit.compose.ui.domain.repository.SchoolRepository
+import com.nocholla.nyc.schools.hilt.retrofit.compose.ui.domain.model.School
+import com.nocholla.nyc.schools.hilt.retrofit.compose.ui.domain.model.Score
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class SchoolRepositoryImpl @Inject constructor(
+    private val schoolsApiService: SchoolsApiService
+) : SchoolRepository {
+
+    override suspend fun getSchools(): Flow<List<School>> = flow {
+        val schoolDtos = schoolsApiService.getSchools()
+        emit(schoolDtos.map { it.toDomain() })
+    }
+
+    override suspend fun getScores(): Flow<List<Score>> = flow {
+        val scoreDtos = schoolsApiService.getScores()
+        emit(scoreDtos.map { it.toDomain() })
+    }
 }
