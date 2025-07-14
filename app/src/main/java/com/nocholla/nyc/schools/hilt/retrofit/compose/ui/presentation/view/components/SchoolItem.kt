5325ae432dfca70d390b23ad7e4309a8afa4cc8f
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2025-07-13 19:50:48 -0700

    Update SchoolItem.kt

--- app/src/main/java/com/nocholla/nyc/schools/hilt/retrofit/compose/ui/presentation/view/components/SchoolItem.kt
+++ app/src/main/java/com/nocholla/nyc/schools/hilt/retrofit/compose/ui/presentation/view/components/SchoolItem.kt
@@ -1,4 +1,25 @@
 package com.nocholla.nyc.schools.hilt.retrofit.compose.ui.presentation.view.components
 
-class SchoolItem {
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.nocholla.nyc.schools.hilt.retrofit.compose.ui.domain.model.School
+
+@Composable
+fun SchoolItem(school: School, onClick: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .clickable(onClick = onClick)
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = school.schoolName)
+            Text(text = school.overviewParagraph)
+        }
+    }
 }
