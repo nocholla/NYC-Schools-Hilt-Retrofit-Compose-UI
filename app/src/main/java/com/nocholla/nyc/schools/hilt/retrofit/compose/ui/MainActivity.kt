bc82563e346c942f195c03e4bea0a43f6b66d83b
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2025-07-13 18:31:03 -0700

    Update MainActivity.kt

--- app/src/main/java/com/nocholla/nyc/schools/hilt/retrofit/compose/ui/MainActivity.kt
+++ app/src/main/java/com/nocholla/nyc/schools/hilt/retrofit/compose/ui/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.nocholla.nyc.schools.hilt.retrofit.compose.ui.ui.theme.NYCSchoolsHiltRetrofitComposeUITheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
