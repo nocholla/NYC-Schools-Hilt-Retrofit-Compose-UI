c9fb51be4455a1fd8dc5e9f5e92d4475de3baa26
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2025-07-13 19:47:49 -0700

    Update GetScoresUseCase.kt

--- app/src/main/java/com/nocholla/nyc/schools/hilt/retrofit/compose/ui/domain/usecase/GetScoresUseCase.kt
+++ app/src/main/java/com/nocholla/nyc/schools/hilt/retrofit/compose/ui/domain/usecase/GetScoresUseCase.kt
@@ -1,4 +1,22 @@
 package com.nocholla.nyc.schools.hilt.retrofit.compose.ui.domain.usecase
 
-class GetScoresUseCase {
+import com.nocholla.nyc.schools.hilt.retrofit.compose.ui.domain.model.Score
+import com.nocholla.nyc.schools.hilt.retrofit.compose.ui.domain.repository.SchoolRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class GetScoresUseCase @Inject constructor(private val repository: SchoolRepository) {
+    suspend operator fun invoke(): Result<Flow<List<Score>>> {
+        return try {
+            val scoresFlow = repository.getScores().map { scores ->
+                if (scores.isEmpty()) throw Exception("No scores found")
+                scores
+            }.catch { e -> throw e }
+            Result.success(scoresFlow)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
 }
