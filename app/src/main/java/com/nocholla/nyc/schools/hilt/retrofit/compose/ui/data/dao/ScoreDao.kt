dcbf7865df26b978dfb5d9ac04e601e20e544fe1
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2025-07-15 19:23:44 -0700

    Create ScoreDao.kt

---
+++ app/src/main/java/com/nocholla/nyc/schools/hilt/retrofit/compose/ui/data/dao/ScoreDao.kt
@@ -0,0 +1,22 @@
+package com.nocholla.nyc.schools.hilt.retrofit.compose.ui.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.nocholla.nyc.schools.hilt.retrofit.compose.ui.data.model.ScoreEntity
+
+@Dao
+interface ScoreDao {
+    @Query("SELECT * FROM scores")
+    suspend fun getAllScores(): List<ScoreEntity>
+
+    @Query("SELECT * FROM scores WHERE dbn = :dbn")
+    suspend fun getScoreByDbn(dbn: String): ScoreEntity?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAllScores(scores: List<ScoreEntity>)
+
+    @Query("DELETE FROM scores")
+    suspend fun deleteAllScores()
+}
